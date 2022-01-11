778f9d56fb98cfd1cd560056c0c0459363906c9f
mgoulman <moadgoulman@gmail.com>
2022-01-11 14:13:50 +0100

    Navigation Bar design in progress ...

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 import './navbar.css';
+import logo from '../../assets/logo.svg';
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className="gpt3__navbar">
+            <div className="gpt3__navbar-links">
+                <div className="gpt3__navbar-links_logo">
+                    <img src={logo} alt="logo"/>
+                </div>
+                <div className="gpt3__navbar-links_container">
+                    <p><a href="#home" ></a>Home</p>
+                    <p><a href="#wgpt3" ></a>What is GPT3?</p>
+                    <p><a href="#possibility" ></a>Open AI</p>
+                    <p><a href="#features" ></a>Case Studies</p>
+                    <p><a href="#blog" ></a>Library</p>
+                </div>
+            </div>
+            <div className="gpt3__navbar-sign">
+                <p>Sign In</p>
+                <button type="button">Sign Up</button>
+            </div>
         </div>
     )
 }
