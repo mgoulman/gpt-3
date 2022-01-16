f74075bc806e27820faa99e8514567ff54bf4c5b
mgoulman <moadgoulman@gmail.com>
2022-01-16 22:28:30 +0100

    add the features and possibility sections + blog

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,53 @@
 import React from 'react';
 import './blog.css';
+import { Article } from '../../components';
+import {blog01, blog02, blog03,blog04, blog05} from './imports';
+
+const article = {
+    image: blog01,
+    date: "Sep 26, 2021",
+    title: "GPT-3 and Open  AI is the future. Let us exlore how it is?"
+};
+
+const articles = [
+    {
+        image: blog02,
+        date: "Sep 26, 2021",
+        title: "GPT-3 and Open  AI is the future. Let us exlore how it is?"
+    },
+    {
+        image: blog03,
+        date: "Sep 26, 2021",
+        title: "GPT-3 and Open  AI is the future. Let us exlore how it is?"
+    },
+    {
+        image: blog04,
+        date: "Sep 26, 2021",
+        title: "GPT-3 and Open  AI is the future. Let us exlore how it is?"
+    },
+    {
+        image: blog05,
+        date: "Sep 26, 2021",
+        title: "GPT-3 and Open  AI is the future. Let us exlore how it is?"
+    },
+]
 
 const Blog = () => {
     return (
-        <div>
-            
+        <div className="gpt3__blog section__padding" id="blog">
+            <div className="gpt3__blog-heading">
+                <h1 className="gradient__text">A lot is happening, We are blogging about it.</h1>
+            </div>
+            <div className="gpt3__blog-container">
+                <div className="gpt3__blog-container__groupA">
+                    <Article image={article.image} date={article.date} title={article.title}/>
+                </div>
+                <div className="gpt3__blog-container_groupB">
+                    {articles.map((item, index) => (
+                        <Article image={item.image} date={item.date} title={item.title} key={item.title + index}/>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -3,14 +3,28 @@ import './whatGPT3.css';
 import { Feature } from '../../components'
 
 const WhatGPT3 = () => {
+
+    const title_1 = "What is GPT-3";
+    const title_2 = "Chatbots";
+    const title_3 = "Knowledgebase";
+    const title_4 = "Education";
+    const text_1 = "We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."; 
+    const text_2 = "We so opinion friends me message as delight. Whole front do of plate heard oh ought. ";
+    const text_3 = "At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b";
+    const text_4 = "At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b";
     return (
-        <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+        <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
             <div className="gpt3__whatgpt3-feature">
-                <Feature />
+                <Feature title={title_1} text={text_1}/>
             </div>
             <div className="gpt3__whatgpt3-heading">
-                <h1 className="gradient-text">The possibilities are beyond your imagination</h1>
+                <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
                 <p>Explore The Library</p>
+            </div>
+            <div className="gpt3__whatgpt3-container">
+                <Feature title={title_2} text={text_2}/>
+                <Feature title={title_3} text={text_3}/>
+                <Feature title={title_4} text={text_4}/>
             </div>
         </div>
     )
